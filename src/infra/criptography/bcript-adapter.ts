f13c115ffcb82fab0ff05059e3bebb0fe72cff55
Alefe Chorna <alefe@twoweb.com.br>
2020-07-08 13:04:41 -0300

    feat: add SignUp Factory

--- src/infra/criptography/bcript-adapter.ts
+++ src/infra/criptography/bcript-adapter.ts
@@ -4,8 +4,9 @@ import { Encrypter } from '../../data/protocols/encrypter'
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number
 
-  constructor (salt: number) {
-    this.salt = salt
+  constructor (salt?: number) {
+    const defaultSalt = 12
+    this.salt = salt || defaultSalt
   }
 
   async encrypt (value: string): Promise<string> {
